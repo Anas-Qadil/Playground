9c9ecb7f591ae3f2245142edcaef6596318ac8a4
Anas Qadil <qadilanass10@gmail.com>
2023-01-10 23:30:38 +0100

    enough sleeping time to get serious

---
+++ algorithms/trees/trees.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+class Node {
+	public: 
+		std::string name;
+};
+
+
+class Tree {
+	public:
+		Node node;
+};
+
+
+int main(int argc, char **argv) {
+
+	Tree t;
+
+	std::cout << "hello world" << std::endl;
+	return (0);
+}
+
